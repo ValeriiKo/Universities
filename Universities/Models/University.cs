3f4868060c3591041cbbfb9fb7f7bbf3d36d5676
Valerii <valeriikozhevnikov@gmail.com>
2017-08-15 12:51:17 +0300

    Создан простой класс доменной модели

---
+++ Universities/Models/University.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Universities.Models
+{
+    public class University
+    {
+        public int UniversityId { get; set; }
+        public string Name { get; set; }
+        public int YearEstablishment { get; set; }
+        public int Description { get; set; }
+    }
+}
